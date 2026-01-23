0431916effcae882f383f963477e89cb160d0d2e
Yuqi Liu <yuqiliu617@outlook.com>
2026-01-22 22:40:09 -0800

    feat(root): initialize repository

---
+++ contracts/delegate-lock/src/main.rs
@@ -0,0 +1,35 @@
+#![cfg_attr(not(any(feature = "library", test)), no_std)]
+#![cfg_attr(not(test), no_main)]
+
+#[cfg(any(feature = "library", test))]
+extern crate alloc;
+
+mod error;
+
+use ckb_std::debug;
+use error::Error;
+
+#[cfg(not(any(feature = "library", test)))]
+ckb_std::entry!(program_entry);
+#[cfg(not(any(feature = "library", test)))]
+// By default, the following heap configuration is used:
+// * 16KB fixed heap
+// * 1.2MB(rounded up to be 16-byte aligned) dynamic heap
+// * Minimal memory block in dynamic heap is 64 bytes
+// For more details, please refer to ckb-std's default_alloc macro
+// and the buddy-alloc alloc implementation.
+ckb_std::default_alloc!(16384, 1258306, 64);
+
+pub fn program_entry() -> i8 {
+    match run() {
+        Ok(_) => 0,
+        Err(e) => {
+            debug!("Error: {:?}", e);
+            e as i8
+        }
+    }
+}
+
+fn run() -> Result<(), Error> {
+    todo!("implement delegate-lock logic");
+}
